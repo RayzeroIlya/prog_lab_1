a4a1f06c349430fbd89e7d25f422a255e7de140c
Ilya <raythezero2905@yandex.ru>
2024-10-23 02:47:00 +0700

    single list ready

--- linkedlist.h
+++ linkedlist.h
@@ -5,23 +5,81 @@ struct Node{
     int data;
     Node* next;
     Node(int _data): data(_data) , next(nullptr){}
+    Node() : next(nullptr) {}
 
 };
 struct LinkedList{
     Node* head;
     Node* tail;
-    LinkedList(): head(nullptr), tail(nullptr){}
+    LinkedList(): head(nullptr){}
 
     void LINSERT(const int& data){
+        Node* newNode = new Node(data);
         if (head == nullptr){
-            head->data=data;
-            head->next=tail;
+            head = newNode;
+            tail= newNode;
             return;
         }
-        Node* temp = head;
-        head=new Node(data);
+        Node* temp=head;
+        head=newNode;
         head->next=temp;
         return;
     }
+    void LPUSH(const int& _data) {
+        Node* newNode = new Node(_data);
+                if (head == nullptr){
+            head = newNode;
+            tail= newNode;
+            return;
+        }
+        tail->next=newNode;
+        tail=newNode;
+        return;
+    }
+    void LDELHEAD(){
+            if (head == nullptr) return;
+            Node *temp=head;
+            head=temp->next;
+            delete temp;
+    }
+    void LDELTAIL(){
+        if (head==nullptr) return;
+        if(head==tail){
+            LDELHEAD();
+            return;
+        }
+        Node* temp=head;
+        while (temp->next!=tail) temp=temp->next;
+        temp->next=nullptr;
+        delete tail;
+        tail=temp;
+    }
+    void LDEL(const int& _data){
+        if (head==nullptr) return;
+        if(head==tail){
+            LDELHEAD();
+            return;
+        }else if (tail->data==_data){
+            LDELTAIL();
+            return;
+        }
+    Node* one=head;
+    Node* two = head->next;
+    while (two && two->data!=_data) { //идем пока two есть и его дата не равна искомой
+        one=one->next;
+        two=two->next;
+    }
+    if(!two) {
+        cout << "Такого элемента нет"<< endl;
+        return;
+    }
+    one->next=two->next;
+    delete two;
+    }
+    Node* LFIND(const int& _data){
+        Node* temp=head;
+        while (temp && temp->data!=_data) temp=temp->next;
+        return (temp&&temp->data==_data) ? temp : nullptr; //Если нода есть и дата равна искомой
 
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
 #include <iostream>
 using namespace std;
 #include "array.h"
-
+#include "linkedlist.h"
 
 int main(){
-    Array myArray;
-    myArray.size=2;
-    myArray.arr=new int[myArray.size];
-    myArray.MINSERT(0,9);
-    myArray.MINSERT(1,15);
-    for (int i=0;i<myArray.size;i++) cout << myArray.arr[i] << " ";
-    myArray.MPUSH(27);
-    cout <<endl;
-        for (int i=0;i<myArray.size;i++) cout << myArray.arr[i] << " ";
-    cout <<endl;
-    cout << myArray.AT(1) << endl;
-
-    myArray.MDELETEAT(1);
-    for (int i=0;i<myArray.size;i++) cout <<myArray.arr[i] << " ";
-    cout << endl;
-    cout << myArray.SIZE();
+    
 }
