268c332f0c2d1da91ad3938d3eeefe22451b9975
Ilya <raythezero2905@yandex.ru>
2024-10-23 04:19:07 +0700

    queue ready

--- queue.h
+++ queue.h
@@ -3,3 +3,24 @@
 #include "node.h"
 using namespace std;
 
+struct QUEUE{
+    Node* first;
+    Node* last;
+    QUEUE() : first(nullptr),last(nullptr){}
+    Node* QPOP() {
+        if (first==nullptr) return;
+        Node* temp=first;
+        first=first->next;
+        if(first==nullptr) last=nullptr; 
+        return temp ;
+    }
+    void QPUSH(const int& _data) {
+        Node* newNode=new Node(_data);
+        if (first == nullptr){
+            first=last=newNode;
+        }
+        last->next=newNode;
+        last=newNode;
+    }
+
+};
